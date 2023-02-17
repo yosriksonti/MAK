aac0daf2de143e44ad313707675d261a5521c0e0
yosriksonti <45453718+yosriksonti@users.noreply.github.com>
2023-02-17 13:01:46 +0100

    fix agence

--- src/Form/AgenceType.php
+++ src/Form/AgenceType.php
@@ -6,6 +6,7 @@ use App\Entity\Agence;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class AgenceType extends AbstractType
 {
