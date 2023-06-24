4b2c35caaf40be3e363a720c2e7dbfad5e66ac7c
yosriksonti <45453718+yosriksonti@users.noreply.github.com>
2023-06-24 16:14:12 +0100

    ChoiceType

--- src/Form/PaymentType.php
+++ src/Form/PaymentType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PaymentType extends AbstractType
